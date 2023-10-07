5d775fb8db3c8dc41f31c2852eb526cee5a9dbff
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-07 11:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-unction decimalToBinary(num) {
-  if (num === 0) {
-    return "0"; // Special case for 0
+function decimalToBinary(decimalNum) {
+  if (decimalNum === 0) {
+    return "0";
   }
 
-  let binary = "";
-  while (num > 0) {
-    const remainder = num % 2;
-    binary = remainder + binary;
-    num = Math.floor(num / 2);
+  let binaryNum = "";
+  while (decimalNum > 0) {
+    const remainder = decimalNum % 2;
+    binaryNum = remainder + binaryNum;
+    decimalNum = Math.floor(decimalNum / 2);
   }
 
-  return binary;
+  return binaryNum;
 }
 
-window.decimalToBinary = decimalToBinary;
+// Example usage:
+const input1 = 7;
+const input2 = 10;
+const input3 = 33;
+
+const output1 = decimalToBinary(input1);
+const output2 = decimalToBinary(input2);
+const output3 = decimalToBinary(input3);
+
+console.log(`Input: ${input1}, Output: ${output1}`);
+console.log(`Input: ${input2}, Output: ${output2}`);
+console.log(`Input: ${input3}, Output: ${output3}`);
